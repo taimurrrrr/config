ad241a75c8b8be2e8b94831931d5329b0248622a
taimurrrrr <142719427+taimurrrrr@users.noreply.github.com>
2023-09-01 16:59:46 +0800

    Add files via upload

---
+++ psenet_r50_msra.py
@@ -0,0 +1,60 @@
+model = dict(
+    type='PSENet',
+    backbone=dict(
+        type='resnet50',
+        pretrained=True
+    ),
+    neck=dict(
+        type='ATE_FPN',
+        in_channels=(256, 512, 1024, 2048),
+        out_channels=128
+    ),
+
+    detection_head=dict(
+            type='PSENet_Head',
+            in_channels=1024,
+            hidden_dim=256,
+            num_classes=7,
+            loss_text=dict(
+                type='DiceLoss',
+                loss_weight=0.7
+            ),
+            loss_kernel=dict(
+                type='DiceLoss',
+                loss_weight=0.3
+            ),
+        )
+)
+data = dict(
+    batch_size=4,
+    train=dict(
+        type='PSENET_MSRA',
+        split='train',
+        is_transform=True,
+        img_size=640,
+        short_size=640,
+        kernel_num=7,
+        min_scale=0.7,
+        read_type='cv2'
+    ),
+    test=dict(
+        type='PSENET_MSRA',
+        split='test',
+        short_size=640,
+        read_type='cv2'
+    )
+)
+train_cfg = dict(
+    lr=1e-3,
+    schedule=(200, 400,),
+    epoch=600,
+    optimizer='SGD'
+
+)
+test_cfg = dict(
+    min_score=0.86,
+    min_area=16,
+    bbox_type='rect',
+    kernel_num=7,
+    result_path='outputs/submit_msra/'
+)
